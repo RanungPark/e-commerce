d19986f21b8a15d6d3563c10fac08cf18b34561a
woong <ckehfqkr302@naver.com>
2024-04-14 23:32:32 +0900

    타이포 그래피 타입 설정

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,6 +1,22 @@
 import "styled-components";
 
 declare module "styled-components" {
+  export type IStyleName =
+    | "Heading1"
+    | "Heading2"
+    | "Heading3"
+    | "Heading4"
+    | "Heading5"
+    | "Heading6"
+    | "Subtitle"
+    | "Body"
+    | "Button"
+    | "Links"
+    | "OverLine"
+    | "Caption"
+    | "CaptionSmall"
+    | "CaptionBold";
+
   export interface DefaultTheme {
     colors: {
       white: string;
@@ -13,5 +29,6 @@ declare module "styled-components" {
       error: string;
     };
     flexSet: (direction?: string, just?: string, align?: string) => string;
+    typography: (styleName: IStyleName) => string;
   }
 }
