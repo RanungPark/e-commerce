73ece6e633db94559c5a339feda488b0de934169
woong <ckehfqkr302@naver.com>
2024-04-14 23:32:18 +0900

    타이포 그래피 설정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,4 @@
-import { DefaultTheme } from "styled-components";
+import { DefaultTheme, IStyleName } from "styled-components";
 
 const colors = {
   white: "#ffffff",
@@ -11,14 +11,106 @@ const colors = {
   error: "#f55f56",
 };
 
-const flexSet = (direction = "column", just = "center", align = "center") => {
+const flexSet = (direction = "row", just = "center", align = "center") => {
   return `display: flex;
   flex-direction: ${direction};
   justify-content: ${just};
   align-items: ${align};`;
 };
 
+const typography = (styleName: IStyleName) => {
+  switch (styleName) {
+    case "Heading1":
+      return `
+      font-size: 67px;
+      line-height: 120%;
+      font-weight: 600;
+    `;
+    case "Heading2":
+      return `
+      font-size: 50px;
+      line-height: 120%;
+      font-weight: 600;
+    `;
+    case "Heading3":
+      return `
+      font-size: 38px;
+      line-height: 120%;
+      font-weight: 500;
+    `;
+    case "Heading4":
+      return `
+      font-size: 28px;
+      line-height: 120%;
+      font-weight: 500;
+    `;
+    case "Heading5":
+      return `
+      font-size: 21px;
+      line-height: 120%;
+      font-weight: 500;
+    `;
+    case "Heading6":
+      return `
+      font-size: 16px;
+      line-height: 120%;
+      font-weight: 500;
+    `;
+    case "Subtitle":
+      return `
+      font-size: 18px;
+      line-height: 120%;
+      font-weight: 500;
+    `;
+    case "Body":
+      return `
+      font-size: 16px;
+      line-height: 140%;
+      font-weight: 400;
+    `;
+    case "Button":
+      return `
+      font-size: 16px;
+      line-height: 120%;
+      font-weight: 500;
+      text-transform: uppercase;
+    `;
+    case "Links":
+      return `
+      font-size: 16px;
+      line-height: 120%;
+      font-weight: 500;
+    `;
+    case "OverLine":
+      return `
+      font-size: 14px;
+      line-height: 120%;
+      font-weight: 500;
+      text-transform: uppercase;
+    `;
+    case "Caption":
+      return `
+      font-size: 14px;
+      line-height: 120%;
+      font-weight: 400;
+    `;
+    case "CaptionSmall":
+      return `
+      font-size: 12px;
+      line-height: 120%;
+      font-weight: 500;
+    `;
+    case "CaptionBold":
+      return `
+      font-size: 14px;
+      line-height: 120%;
+      font-weight: 500;
+      `;
+  }
+};
+
 export const theme: DefaultTheme = {
   colors,
   flexSet,
+  typography,
 };
