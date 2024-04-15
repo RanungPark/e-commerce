e1cc925cbf5f0dd6d142e29857d5fadd77788584
woong <ckehfqkr302@naver.com>
2024-04-15 19:02:35 +0900

    스템 인디케이털 컴포넌트

---
+++ src/components/StepIndicator.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+import { ReactComponent as ChevronRight } from "../assets/icons/wght400/chevron-right.svg";
+
+const StyleStepIndicator = styled.div`
+  ${({ theme }) => theme.typography("OverLine")}
+  ${({ theme }) => theme.flexSet(undefined, undefined, "flex-start")};
+  cursor: pointer;
+  color: ${({ theme }) => theme.colors.gray};
+
+  & span {
+    margin-right: 8px;
+  }
+
+  & path {
+    padding: 0;
+    fill: ${({ theme }) => theme.colors.gray};
+  }
+
+  &:active {
+    color: ${({ theme }) => theme.colors.black};
+
+    & path {
+      fill: ${({ theme }) => theme.colors.black};
+    }
+  }
+`;
+
+const StepIndicator = () => {
+  return (
+    <StyleStepIndicator>
+      <span>{"INFORMATION"}</span>
+      <div>
+        <ChevronRight />
+      </div>
+    </StyleStepIndicator>
+  );
+};
+
+export default StepIndicator;
