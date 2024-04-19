b11a20e000352e1d0f2142ad48fd65219eb3b6e1
woong <ckehfqkr302@naver.com>
2024-04-19 16:12:35 +0900

    eslint 적용

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,13 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<GlobalStyle />
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>,
 );
