ff3cf2a6331a0ed941cc16e643aa6df4a1093ad0
woong <ckehfqkr302@naver.com>
2024-04-19 16:13:40 +0900

    텍스트 아이콘 버튼 인터페이스 지정

--- src/components/TextIconButton.tsx
+++ src/components/TextIconButton.tsx
@@ -1,48 +1,77 @@
 import React from 'react';
 import styled from 'styled-components';
-import { ReactComponent as ArrowRight } from '../assets/icons/wght400/chevron-right.svg';
+import { ReactComponent as East300 } from '../assets/icons/wght300/east_wght300_opsz24.svg';
+import { ReactComponent as West300 } from '../assets/icons/wght300/west_300_opsz24.svg';
 
-const StyleTextIconButton = styled.button`
-  ${({ theme }) => theme.flexSet()}
-  ${({ theme }) => theme.typography('Links')}
-  color: ${({ theme }) => theme.colors.black};
-  border: none;
-  background-color: inherit;
-  gap: 4px;
-  position: relative;
-  cursor: pointer;
+const StyledTextIconButton = styled.button`
+	${({ theme }) => theme.flexSet()}
+	${({ theme }) => theme.typography('Links')}
+  width: auto;
+	height: auto;
+	padding: 0;
+	border: none;
+	color: ${({ theme }) => theme.colors.black};
+	background-color: inherit;
+	gap: 4px;
+	position: relative;
+	cursor: pointer;
 
-  &:hover {
-    color: ${({ theme }) => theme.colors.gray};
-  }
+	&:hover {
+		color: ${({ theme }) => theme.colors.gray};
+	}
 
-  &:active {
-    color: ${({ theme }) => theme.colors.black};
-  }
+	&:active {
+		color: ${({ theme }) => theme.colors.black};
+	}
 
-  &:disabled {
-    color: ${({ theme }) => theme.colors.lightgray};
-  }
+	&:disabled {
+		color: ${({ theme }) => theme.colors.lightgray};
+		cursor: not-allowed;
+	}
 
-  &:hover::after,
-  &:active::after {
-    content: '';
-    position: absolute;
-    left: 0;
-    bottom: -2px;
-    width: 100%;
-    height: 1px;
-    background-color: ${({ theme }) => theme.colors.black};
-  }
+	&:hover::after,
+	&:active::after {
+		content: '';
+		position: absolute;
+		left: 0;
+		bottom: -2px;
+		width: 100%;
+		height: 1px;
+		background-color: ${({ theme }) => theme.colors.black};
+	}
 `;
 
-const TextIconButton = () => {
-  return (
-    <StyleTextIconButton>
-      {'Button'}
-      <ArrowRight />
-    </StyleTextIconButton>
-  );
+interface ITextIconButton {
+	text: string;
+	arrowIcon?: 'rightIcon' | 'leftIcon' | 'withText';
+	textOrIcon?: 'withText' | 'onlyIcon';
+	isDisabled?: true | false;
+}
+
+const TextIconButton = ({
+	text,
+	arrowIcon = 'rightIcon',
+	textOrIcon = 'withText',
+	isDisabled = false,
+}: ITextIconButton) => {
+	return (
+		<StyledTextIconButton disabled={isDisabled}>
+			{textOrIcon === 'withText' && arrowIcon === 'withText' && <>{text}</>}
+			{textOrIcon === 'onlyIcon' && (
+				<>
+					{arrowIcon === 'leftIcon' && <West300 />}
+					{arrowIcon === 'rightIcon' && <East300 />}
+				</>
+			)}
+			{textOrIcon === 'withText' && arrowIcon !== 'withText' && (
+				<>
+					{arrowIcon === 'leftIcon' && <West300 />}
+					{text}
+					{arrowIcon === 'rightIcon' && <East300 />}
+				</>
+			)}
+		</StyledTextIconButton>
+	);
 };
 
 export default TextIconButton;
