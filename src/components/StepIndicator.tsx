e847c6a7cacb17e5ec41499ca78658f96ff79770
woong <ckehfqkr302@naver.com>
2024-04-20 22:52:03 +0900

    높이 너비 추가

--- src/components/StepIndicator.tsx
+++ src/components/StepIndicator.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import { ReactComponent as ChevronRight } from '../assets/icons/wght400/chevron-right.svg';
 
-const StyleStepIndicator = styled.div`
+const StyledStepIndicator = styled.div`
   ${({ theme }) => theme.typography('OverLine')}
   ${({ theme }) => theme.flexSet(undefined, undefined, 'flex-start')};
+  width: auto;
+  height: auto;
   cursor: pointer;
   color: ${({ theme }) => theme.colors.gray};
 
@@ -28,12 +30,12 @@ const StyleStepIndicator = styled.div`
 
 const StepIndicator = () => {
   return (
-    <StyleStepIndicator>
+    <StyledStepIndicator>
       <span>{'INFORMATION'}</span>
       <div>
         <ChevronRight />
       </div>
-    </StyleStepIndicator>
+    </StyledStepIndicator>
   );
 };
 
