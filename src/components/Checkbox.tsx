6cccf73dd0e20d548e6d3a7b482009c0f45f2fcb
woong <ckehfqkr302@naver.com>
2024-04-20 22:51:24 +0900

    타입 지정과 상태에 따른 변화

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import checkbox from '../assets/icons/wght300/check_FILL0_wght300_GRAD0_opsz24.svg';
+import { ReactComponent as Check300 } from '../assets/icons/wght300/check_FILL0_wght300_GRAD0_opsz24.svg';
 
-const StyleCheckbox = styled.label`
+const StyledCheckbox = styled.label`
   ${({ theme }) => theme.typography('CaptionBold')};
   position: relative;
+  width: auto;
+  height: auto;
+  color: ${({ theme }) => theme.colors.black};
   cursor: pointer;
 
+  & path {
+    fill: ${({ theme }) => theme.colors.black};
+  }
+
   & [type='checkbox'],
-  span {
+  p {
     vertical-align: text-top;
+  }
+
+  p {
+    display: inline-block;
   }
 
   & [type='checkbox'] {
@@ -18,27 +29,48 @@ const StyleCheckbox = styled.label`
     border: 1px solid ${({ theme }) => theme.colors.black};
     width: 24px;
     height: 24px;
-    cursor: pointer;
     margin-right: 12px;
 
     &:checked {
-      & + img {
+      & + svg {
         position: absolute;
         top: 20%;
         left: 4%;
       }
     }
   }
+
+  &:active {
+    color: ${({ theme }) => theme.colors.gray};
+
+    & [type='checkbox'] {
+      border: 1px solid ${({ theme }) => theme.colors.darkgray};
+    }
+
+    & path {
+      fill: ${({ theme }) => theme.colors.gray};
+    }
+  }
 `;
 
-const Checkbox = () => {
-  const check = true;
+interface ICheckbox {
+  text: string;
+}
+
+const Checkbox = ({ text }: ICheckbox) => {
+  const [check, setCheck] = useState(true);
+
+  const handleClick = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    e.preventDefault();
+    setCheck(!check);
+  };
+
   return (
-    <StyleCheckbox>
-      <input type="checkbox" value="checkbox" checked />
-      {check ? <img src={checkbox} alt="checkbox Icon" /> : <></>}
-      <span>{'Checkbox'}</span>
-    </StyleCheckbox>
+    <StyledCheckbox onClick={handleClick}>
+      <input type="checkbox" value="checkbox" checked={check} />
+      {check && <Check300 />}
+      <p>{text}</p>
+    </StyledCheckbox>
   );
 };
 
