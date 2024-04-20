76b8e9199e08d76b1f35c7e8ee91a8fb19b30d93
woong <ckehfqkr302@naver.com>
2024-04-20 22:51:04 +0900

    boolean으로 변경

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,168 +4,168 @@ import { ReactComponent as East300 } from '../assets/icons/wght300/east_wght300_
 import { ReactComponent as West300 } from '../assets/icons/wght300/west_300_opsz24.svg';
 
 const StyledButton = styled.button<{
-	colorType: 'primary' | 'secondary' | 'tertiary';
+  colorType: 'primary' | 'secondary' | 'tertiary';
 }>`
-	${({ theme }) => theme.flexSet()}
-	${({ theme }) => theme.typography('Button')}
+  ${({ theme }) => theme.flexSet()}
+  ${({ theme }) => theme.typography('Button')}
   padding: 16px 24px 14px 24px;
-	min-width: 175px;
-	width: 100%;
-	height: 56px;
-	cursor: pointer;
-	gap: 8px;
-	background-color: ${({ colorType, theme }) => {
-		switch (colorType) {
-			case 'primary':
-				return theme.colors.black;
-			case 'secondary':
-				return theme.colors.white;
-			case 'tertiary':
-				return 'inherit';
-		}
-	}};
-	color: ${({ colorType, theme }) => {
-		switch (colorType) {
-			case 'primary':
-				return theme.colors.white;
-			case 'secondary':
-				return theme.colors.black;
-			case 'tertiary':
-				return theme.colors.white;
-		}
-	}};
-	border: ${({ colorType, theme }) => {
-		switch (colorType) {
-			case 'primary':
-				return 'none';
-			case 'secondary':
-				return `1px solid ${theme.colors.black}`;
-			case 'tertiary':
-				return `1px solid ${theme.colors.white}`;
-		}
-	}};
+  min-width: 175px;
+  width: 100%;
+  height: 56px;
+  cursor: pointer;
+  gap: 8px;
+  background-color: ${({ colorType, theme }) => {
+    switch (colorType) {
+      case 'primary':
+        return theme.colors.black;
+      case 'secondary':
+        return theme.colors.white;
+      case 'tertiary':
+        return 'inherit';
+    }
+  }};
+  color: ${({ colorType, theme }) => {
+    switch (colorType) {
+      case 'primary':
+        return theme.colors.white;
+      case 'secondary':
+        return theme.colors.black;
+      case 'tertiary':
+        return theme.colors.white;
+    }
+  }};
+  border: ${({ colorType, theme }) => {
+    switch (colorType) {
+      case 'primary':
+        return 'none';
+      case 'secondary':
+        return `1px solid ${theme.colors.black}`;
+      case 'tertiary':
+        return `1px solid ${theme.colors.white}`;
+    }
+  }};
 
-	& path {
-		fill: ${({ colorType, theme }) => {
-			switch (colorType) {
-				case 'primary':
-					return theme.colors.white;
-				case 'secondary':
-					return theme.colors.black;
-				case 'tertiary':
-					return theme.colors.white;
-			}
-		}};
-	}
+  & path {
+    fill: ${({ colorType, theme }) => {
+      switch (colorType) {
+        case 'primary':
+          return theme.colors.white;
+        case 'secondary':
+          return theme.colors.black;
+        case 'tertiary':
+          return theme.colors.white;
+      }
+    }};
+  }
 
-	&:hover {
-		background-color: ${({ colorType, theme }) => {
-			switch (colorType) {
-				case 'primary':
-					return theme.colors.darkgray;
-				case 'secondary':
-					return theme.colors.black;
-				case 'tertiary':
-					return theme.colors.white;
-			}
-		}};
-		color: ${({ colorType, theme }) => {
-			switch (colorType) {
-				case 'primary':
-					return theme.colors.white;
-				case 'secondary':
-					return theme.colors.white;
-				case 'tertiary':
-					return theme.colors.darkgray;
-			}
-		}};
+  &:hover {
+    background-color: ${({ colorType, theme }) => {
+      switch (colorType) {
+        case 'primary':
+          return theme.colors.darkgray;
+        case 'secondary':
+          return theme.colors.black;
+        case 'tertiary':
+          return theme.colors.white;
+      }
+    }};
+    color: ${({ colorType, theme }) => {
+      switch (colorType) {
+        case 'primary':
+          return theme.colors.white;
+        case 'secondary':
+          return theme.colors.white;
+        case 'tertiary':
+          return theme.colors.darkgray;
+      }
+    }};
 
-		& path {
-			fill: ${({ colorType, theme }) => {
-				switch (colorType) {
-					case 'primary':
-						return theme.colors.white;
-					case 'secondary':
-						return theme.colors.white;
-					case 'tertiary':
-						return theme.colors.darkgray;
-				}
-			}};
-		}
-	}
+    & path {
+      fill: ${({ colorType, theme }) => {
+        switch (colorType) {
+          case 'primary':
+            return theme.colors.white;
+          case 'secondary':
+            return theme.colors.white;
+          case 'tertiary':
+            return theme.colors.darkgray;
+        }
+      }};
+    }
+  }
 
-	&:active {
-		background-color: ${({ colorType, theme }) => {
-			switch (colorType) {
-				case 'primary':
-					return theme.colors.black;
-				case 'secondary':
-					return theme.colors.white;
-				case 'tertiary':
-					return theme.colors.white;
-			}
-		}};
-		color: ${({ colorType, theme }) => {
-			switch (colorType) {
-				case 'primary':
-					return theme.colors.white;
-				case 'secondary':
-					return theme.colors.black;
-				case 'tertiary':
-					return theme.colors.black;
-			}
-		}};
-		border: ${({ colorType, theme }) =>
-			colorType === 'secondary' ? `1px solid ${theme.colors.black}` : 'none'};
+  &:active {
+    background-color: ${({ colorType, theme }) => {
+      switch (colorType) {
+        case 'primary':
+          return theme.colors.black;
+        case 'secondary':
+          return theme.colors.white;
+        case 'tertiary':
+          return theme.colors.white;
+      }
+    }};
+    color: ${({ colorType, theme }) => {
+      switch (colorType) {
+        case 'primary':
+          return theme.colors.white;
+        case 'secondary':
+          return theme.colors.black;
+        case 'tertiary':
+          return theme.colors.black;
+      }
+    }};
+    border: ${({ colorType, theme }) =>
+      colorType === 'secondary' ? `1px solid ${theme.colors.black}` : 'none'};
 
-		& path {
-			fill: ${({ colorType, theme }) => {
-				switch (colorType) {
-					case 'primary':
-						return theme.colors.white;
-					case 'secondary':
-						return theme.colors.black;
-					case 'tertiary':
-						return theme.colors.black;
-				}
-			}};
-		}
-	}
+    & path {
+      fill: ${({ colorType, theme }) => {
+        switch (colorType) {
+          case 'primary':
+            return theme.colors.white;
+          case 'secondary':
+            return theme.colors.black;
+          case 'tertiary':
+            return theme.colors.black;
+        }
+      }};
+    }
+  }
 
-	&:disabled {
-		color: ${({ theme }) => theme.colors.gray};
-		background-color: ${({ colorType, theme }) =>
-			colorType === 'primary' ? theme.colors.lightgray : 'inherit'};
-		border: ${({ colorType, theme }) =>
-			colorType !== 'primary' ? `1px solid ${theme.colors.lightgray}` : 'none'};
-		cursor: not-allowed;
+  &:disabled {
+    color: ${({ theme }) => theme.colors.gray};
+    background-color: ${({ colorType, theme }) =>
+      colorType === 'primary' ? theme.colors.lightgray : 'inherit'};
+    border: ${({ colorType, theme }) =>
+      colorType !== 'primary' ? `1px solid ${theme.colors.lightgray}` : 'none'};
+    cursor: not-allowed;
 
-		& path {
-			fill: ${({ theme }) => theme.colors.gray};
-		}
-	}
+    & path {
+      fill: ${({ theme }) => theme.colors.gray};
+    }
+  }
 `;
 
 interface IButton {
-	text: string;
-	colorType?: 'primary' | 'secondary' | 'tertiary';
-	arrowIcon?: 'default' | 'left' | 'right';
-	isDisabled?: true | false;
+  text: string;
+  colorType?: 'primary' | 'secondary' | 'tertiary';
+  arrowIcon?: 'default' | 'left' | 'right';
+  isDisabled?: boolean;
 }
 
 const Button = ({
-	text,
-	colorType = 'primary',
-	arrowIcon = 'default',
-	isDisabled = false,
+  text,
+  colorType = 'primary',
+  arrowIcon = 'default',
+  isDisabled = false,
 }: IButton) => {
-	return (
-		<StyledButton colorType={colorType} disabled={isDisabled}>
-			{arrowIcon === 'left' && <West300 />}
-			{text}
-			{arrowIcon === 'right' && <East300 />}
-		</StyledButton>
-	);
+  return (
+    <StyledButton colorType={colorType} disabled={isDisabled}>
+      {arrowIcon === 'left' && <West300 />}
+      {text}
+      {arrowIcon === 'right' && <East300 />}
+    </StyledButton>
+  );
 };
 
 export default Button;
