3a6ad028f2c8eb6d3d9e3f99e62936eed32522a0
woong <ckehfqkr302@naver.com>
2024-04-21 17:28:21 +0900

    CardItem 컴포넌트

---
+++ src/components/CardItem.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledCardItem = styled.section<{ bgImg: string }>`
+  ${({ theme }) => theme.flexSet()}
+  border-bottom: 1px solid ${({ theme }) => theme.colors.black};
+  border-left: 1px solid ${({ theme }) => theme.colors.black};
+  position: relative;
+  background-image: url(${({ bgImg }) => bgImg});
+  background-size: cover;
+  width: 100%;
+  height: 25vw;
+  padding: 24px;
+`;
+
+const CardItemDescription = styled.div`
+  position: absolute;
+  width: 100%;
+  height: auto;
+  bottom: 24px;
+`;
+
+const Description = styled.p`
+  ${({ theme }) => theme.typography('Caption')};
+  text-align: center;
+  display: block;
+`;
+
+const DescriptionLabel = styled(Description)`
+  ${({ theme }) => theme.typography('Heading6')};
+  color: ${({ theme }) => theme.colors.black};
+  margin-bottom: 4px;
+`;
+const DescriptionPrice = styled(Description)`
+  color: ${({ theme }) => theme.colors.gray};
+`;
+
+interface ICardItem {
+  bgImg: string;
+  label: string;
+  price: string;
+  isDescription?: boolean;
+}
+
+const CardItem = ({ bgImg, label, price, isDescription = true }: ICardItem) => {
+  return (
+    <StyledCardItem bgImg={bgImg}>
+      {isDescription !== false && (
+        <CardItemDescription>
+          <DescriptionLabel>{label}</DescriptionLabel>
+          <DescriptionPrice>{price}</DescriptionPrice>
+        </CardItemDescription>
+      )}
+    </StyledCardItem>
+  );
+};
+
+export default CardItem;
