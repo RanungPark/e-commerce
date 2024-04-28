3f0e02731df5650776394035ed0d5ed1997d3c04
woong <ckehfqkr302@naver.com>
2024-04-28 23:47:56 +0900

    타이포그래피 변경

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,4 @@
-import { DefaultTheme, IStyleName } from 'styled-components';
+import { DefaultTheme, Typography, Weight, css } from 'styled-components';
 
 const colors = {
   white: '#ffffff',
@@ -18,96 +18,87 @@ const flexSet = (direction = 'row', just = 'center', align = 'center') => {
   align-items: ${align};`;
 };
 
-const typography = (styleName: IStyleName) => {
-  switch (styleName) {
-    case 'Heading1':
-      return `
-      font-size: 67px;
-      line-height: 120%;
-      font-weight: 600;
-    `;
-    case 'Heading2':
-      return `
-      font-size: 50px;
-      line-height: 120%;
-      font-weight: 600;
-    `;
-    case 'Heading3':
-      return `
-      font-size: 38px;
-      line-height: 120%;
-      font-weight: 500;
-    `;
-    case 'Heading4':
-      return `
-      font-size: 28px;
-      line-height: 120%;
-      font-weight: 500;
-    `;
-    case 'Heading5':
-      return `
-      font-size: 21px;
-      line-height: 120%;
-      font-weight: 500;
-    `;
-    case 'Heading6':
-      return `
-      font-size: 16px;
-      line-height: 120%;
-      font-weight: 500;
-    `;
-    case 'Subtitle':
-      return `
-      font-size: 18px;
-      line-height: 120%;
-      font-weight: 500;
-    `;
-    case 'Body':
-      return `
-      font-size: 16px;
-      line-height: 140%;
-      font-weight: 400;
-    `;
-    case 'Button':
-      return `
-      font-size: 16px;
-      line-height: 120%;
-      font-weight: 500;
-      text-transform: uppercase;
-    `;
-    case 'Links':
-      return `
-      font-size: 16px;
-      line-height: 120%;
-      font-weight: 500;
-    `;
-    case 'OverLine':
-      return `
-      font-size: 14px;
-      line-height: 120%;
-      font-weight: 500;
-      text-transform: uppercase;
-    `;
-    case 'Caption':
-      return `
-      font-size: 14px;
-      line-height: 120%;
-      font-weight: 400;
-    `;
-    case 'CaptionSmall':
-      return `
-      font-size: 12px;
-      line-height: 120%;
-      font-weight: 500;
-    `;
-    case 'CaptionBold':
-      return `
-      font-size: 14px;
-      line-height: 120%;
-      font-weight: 500;
-      `;
-  }
+const weightScheme: Record<Weight, number> = {
+  Semibold: 600,
+  Medium: 500,
+  Regular: 400,
 };
+
+const sizeMap: Record<Typography, number> = {
+  Heading1: 67,
+  Heading2: 50,
+  Heading3: 38,
+  Heading4: 28,
+  Heading5: 21,
+  Heading6: 16,
+  Subtitle: 18,
+  Body: 16,
+  Button: 16,
+  Links: 16,
+  Overline: 14,
+  Caption: 14,
+  CaptionSmall: 12,
+  CaptionBold: 14,
+};
+
+const lineHeightMap: Record<Typography, number> = {
+  Heading1: 120,
+  Heading2: 120,
+  Heading3: 120,
+  Heading4: 120,
+  Heading5: 120,
+  Heading6: 120,
+  Subtitle: 120,
+  Body: 140,
+  Button: 120,
+  Links: 120,
+  Overline: 120,
+  Caption: 120,
+  CaptionSmall: 120,
+  CaptionBold: 120,
+};
+
+const weightMap: Record<Typography, number> = {
+  Heading1: weightScheme.Semibold,
+  Heading2: weightScheme.Semibold,
+  Heading3: weightScheme.Medium,
+  Heading4: weightScheme.Medium,
+  Heading5: weightScheme.Medium,
+  Heading6: weightScheme.Medium,
+  Subtitle: weightScheme.Medium,
+  Body: weightScheme.Regular,
+  Button: weightScheme.Medium,
+  Links: weightScheme.Medium,
+  Overline: weightScheme.Medium,
+  Caption: weightScheme.Regular,
+  CaptionSmall: weightScheme.Medium,
+  CaptionBold: weightScheme.Medium,
+};
+
+export type RuleSet<T extends Typography> = {
+  [key in T]: string;
+};
+
+function buildTypography(): RuleSet<Typography> {
+  const newTypography: any = {};
+
+  for (const key in sizeMap) {
+    if (sizeMap.hasOwnProperty(key)) {
+      const size = sizeMap[key as Typography];
+      const lineHeight = lineHeightMap[key as Typography];
+      const weight = weightMap[key as Typography];
+      newTypography[
+        key as Typography
+      ] = `font-size: ${size}px; font-weight: ${weight}; line-height: ${lineHeight}%;`;
+    }
+  }
+
+  return newTypography as RuleSet<Typography>;
+}
+
+const typography = buildTypography();
+
+console.log(typography);
 
 export const theme: DefaultTheme = {
   colors,
