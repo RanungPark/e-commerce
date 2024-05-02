5e3b0e83b391e5b2b5ffce4ff6f32db0ac19fbe3
woong <ckehfqkr302@naver.com>
2024-05-02 22:59:25 +0900

    d.ts 파일 변경

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,37 +1,6 @@
 import 'styled-components';
+import { Theme } from './theme';
 
 declare module 'styled-components' {
-  type Typography =
-    | 'Heading1'
-    | 'Heading2'
-    | 'Heading3'
-    | 'Heading4'
-    | 'Heading5'
-    | 'Heading6'
-    | 'Subtitle'
-    | 'Body'
-    | 'Button'
-    | 'Links'
-    | 'Overline'
-    | 'Caption'
-    | 'CaptionSmall'
-    | 'CaptionBold';
-
-  type Weight = 'Semibold' | 'Medium' | 'Regular';
-
-  interface DefaultTheme {
-    colors: {
-      white: string;
-      extralight: string;
-      lightgray: string;
-      gray: string;
-      darkgray: string;
-      black: string;
-      succeess: string;
-      error: string;
-    };
-    typography: {
-      [K in Typography]: string;
-    };
-  }
+  interface DefaultTheme extends Theme {}
 }
