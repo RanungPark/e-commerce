7eaa2aaacaf33a6164b5b48386d1f8ff4b4860be
woong <ckehfqkr302@naver.com>
2024-05-02 23:01:34 +0900

    테마 타입

---
+++ src/@types/theme.ts
@@ -0,0 +1,27 @@
+export type Color =
+  | 'white'
+  | 'extralight'
+  | 'lightgray'
+  | 'gray'
+  | 'darkgray'
+  | 'black'
+  | 'succeess'
+  | 'error';
+
+export type Typography =
+  | 'Heading1'
+  | 'Heading2'
+  | 'Heading3'
+  | 'Heading4'
+  | 'Heading5'
+  | 'Heading6'
+  | 'Subtitle'
+  | 'Body'
+  | 'Button'
+  | 'Links'
+  | 'Overline'
+  | 'Caption'
+  | 'CaptionSmall'
+  | 'CaptionBold';
+
+export type Weight = 'Semibold' | 'Medium' | 'Regular';
