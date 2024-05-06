bfd7eaebb66bd0813985ee20a2b9ab49fb8a32de
woong <ckehfqkr302@naver.com>
2024-05-07 01:24:12 +0900

    specifyColor 추가

--- src/styles/Mixin.ts
+++ src/styles/Mixin.ts
@@ -1,13 +1,36 @@
+import { OptionalSpecifyColorProps } from 'src/@types/mixin/specifyColor';
+import { OptionalFromFlexBoxProps } from 'src/@types/mixin/flexbox';
+import { css } from 'styled-components';
+
 export const mixins = {
-  flexBox: (
+  flexBox: ({
     display = 'flex',
     direction = 'row',
     justify = 'center',
-    align = 'center'
-  ) => `
+    align = 'center',
+  }: OptionalFromFlexBoxProps) => css`
     display: ${display};
     flex-direction: ${direction};
     justify-content: ${justify};
     align-items: ${align};
   `,
+  specifyColor: ({
+    colorAndFill,
+    bgColor,
+    borderColor,
+  }: OptionalSpecifyColorProps) => {
+    return css`
+      ${({ theme }) => {
+        const { colors } = theme;
+        return css`
+          color: ${colors[colorAndFill]};
+          background-color: ${bgColor ? colors[bgColor] : 'inherit'};
+          border: ${borderColor ? `1px solid ${colors[borderColor]}` : `none`};
+          & path {
+            fill: ${colors[colorAndFill]};
+          }
+        `;
+      }}
+    `;
+  },
 };
