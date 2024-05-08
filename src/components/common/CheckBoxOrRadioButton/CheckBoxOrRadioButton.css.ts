d589ae9b15cf79935548e97e58744b1ca4651742
woong <ckehfqkr302@naver.com>
2024-05-08 16:48:23 +0900

    체크박스 또는 라디오 버튼 스타일

---
+++ src/components/common/CheckBoxOrRadioButton/CheckBoxOrRadioButton.css.ts
@@ -0,0 +1,72 @@
+import { css } from "styled-components";
+
+
+export const CheckBoxStyle = css`
+  position: relative;
+
+  & svg {
+    display: none;
+  }
+
+  & input,
+  p {
+    vertical-align: text-top;
+  }
+
+  & input {
+    &:checked {
+      & + svg {
+        display: block;
+        position: absolute;
+        top: 0;
+        left: 0;
+      }
+    }
+  }
+
+  &:active {
+    & input {
+      border: 1px solid ${({ theme }) => theme.colors.darkgray};
+    }
+
+    & path {
+      fill: ${({ theme }) => theme.colors.gray};
+    }
+  }
+`;
+
+export const RadioButtonStyle = css`
+  & input,
+  p {
+    vertical-align: text-top;
+  }
+
+  & input {
+    border-radius: 50%;
+    position: relative;
+
+    &:checked {
+      &::before {
+        content: '';
+        border-radius: 50%;
+        width: 14px;
+        height: 14px;
+        position: absolute;
+        background-color: ${({ theme }) => theme.colors.black};
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      }
+    }
+  }
+
+  &:active {
+    & input {
+      border: 1px solid ${({ theme }) => theme.colors.gray};
+
+      &::before {
+        background-color: ${({ theme }) => theme.colors.gray};
+      }
+    }
+  }
+`;
