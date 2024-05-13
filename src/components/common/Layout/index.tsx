0fd4301dbdcf2ffcde8ed645dc81240d10ab076d
woong <ckehfqkr302@naver.com>
2024-05-13 23:39:25 +0900

    자식만 받게 변경

--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -1,55 +1,11 @@
 import React, { ReactNode } from 'react';
 import styled from 'styled-components';
-import Header from '../Header/Tablet&Mobile';
-import Footer from '../Footer';
-
-const footerData = {
-  remindData: {
-    content: `Remember to offer beautiful flowers from Kyiv LuxeBouquets Valentines
-    Day, Mothers Day, Christmas... Reminds you 7 days before. No spam or
-    sharing your address`,
-    input: {
-      placeholder: 'Your Email',
-    },
-    button: {
-      label: 'reined',
-    },
-  },
-  contactData: {
-    contactInfo: [
-      { title: 'Address', info: '경기도 시흥지 진말로 36' },
-      { title: 'Phone', info: '010-8797-1558' },
-      { title: 'General Enquiry:', info: 'ckehfqkr302@gmail.com' },
-    ],
-  },
-  shopAndServiceDate: {
-    shopInfos: [
-      'All Products',
-      'Fresh Flowers',
-      'Dried Flowers',
-      'Live Plants',
-      'Designer Vases',
-      'Aroma Candles',
-      'Freshener Diffuser',
-    ],
-    serviceInfo: ['Flower Subcription', 'Wedding & Event Decor'],
-  },
-  aboutdata: {
-    aboutInfos: ['Shipping & returns', 'Terms & conditions', 'Privacy policy'],
-  },
-};
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  return (
-    <StyledLayout>
-      <Header />
-      {children}
-      <Footer footerData={footerData} />
-    </StyledLayout>
-  );
+  return <StyledLayout>{children}</StyledLayout>;
 };
 
 const StyledLayout = styled.main`
