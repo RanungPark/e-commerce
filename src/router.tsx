e6dad3beb8fe6aca18f4286b92a644dcf6c552ab
woong <ckehfqkr302@naver.com>
2024-05-13 23:38:36 +0900

    라우터

---
+++ src/router.tsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from 'react-router-dom';
+import App from './App';
+import NotFound from './pages/NotFound';
+import { ReactNode } from 'react';
+import { Routes, routes } from './constants/routes';
+
+interface RouteElement extends Routes {
+  errorElement: ReactNode;
+  children: Routes[];
+}
+
+const routeElement: RouteElement[] = [
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: routes,
+  },
+];
+
+const router = createBrowserRouter(
+  routeElement.map(route => {
+    const childs = route.children?.map(childRoute => {
+      return {
+        path: childRoute.path,
+        element: childRoute.element,
+      };
+    });
+    return {
+      ...route,
+      children: childs,
+    };
+  })
+);
+
+export default router;
