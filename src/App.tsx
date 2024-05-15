b143fd10a49246cfaaecb48235738564788500b7
woong <ckehfqkr302@naver.com>
2024-05-15 21:16:27 +0900

    풋터 데이터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,7 @@ import Layout from './components/common/Layout';
 import Header from './components/common/Header/Tablet&Mobile';
 import Footer from './components/common/Footer';
 import { Outlet } from 'react-router';
-import { useEffect } from 'react';
-
-const footerData = {
-  remindData: {
-    content: `Remember to offer beautiful flowers from Kyiv LuxeBouquets Valentines
-    Day, Mothers Day, Christmas... Reminds you 7 days before. No spam or
-    sharing your address`,
-    input: {
-      placeholder: 'Your Email',
-    },
-    button: {
-      label: 'reined',
-    },
-  },
-  contactData: {
-    contactInfo: [
-      { title: 'Address', info: '경기도 시흥지 진말로 36' },
-      { title: 'Phone', info: '010-8797-1558' },
-      { title: 'General Enquiry:', info: 'ckehfqkr302@gmail.com' },
-    ],
-  },
-  shopAndServiceDate: {
-    shopInfos: [
-      'All Products',
-      'Fresh Flowers',
-      'Dried Flowers',
-      'Live Plants',
-      'Designer Vases',
-      'Aroma Candles',
-      'Freshener Diffuser',
-    ],
-    serviceInfo: ['Flower Subcription', 'Wedding & Event Decor'],
-  },
-  aboutdata: {
-    aboutInfos: ['Shipping & returns', 'Terms & conditions', 'Privacy policy'],
-  },
-};
+import { footerData } from './data/footer';
 
 const App = () => {
   return (
