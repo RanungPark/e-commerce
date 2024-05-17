0c5ff4cd537674b2dd085d9693ddbf0ddf130a2f
woong <ckehfqkr302@naver.com>
2024-05-17 23:05:03 +0900

    버전 추가

--- src/mocks/handlers/outerProducts.ts
+++ src/mocks/handlers/outerProducts.ts
@@ -2,7 +2,7 @@ import { http, HttpResponse } from 'msw';
 import { outerProducts } from '../data/outerProducts';
 
 export const outerProductsHandlers = [
-  http.get('/products/outerProducts', () => {
+  http.get('/v1/products/outerProducts', () => {
     return HttpResponse.json(outerProducts);
   }),
 ];
