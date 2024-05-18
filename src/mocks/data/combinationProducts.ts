797890cdddf50883abc81b6f565ff6dc77fb8257
woong <ckehfqkr302@naver.com>
2024-05-18 23:38:25 +0900

    콤비네이션 상품 데이터

---
+++ src/mocks/data/combinationProducts.ts
@@ -0,0 +1,50 @@
+import { PickFromProducts } from 'src/@types/product';
+import { IMG_PATH } from 'src/constants/imgPath';
+
+export const combinationProducts: PickFromProducts[] = [
+  {
+    name: 'Glass Vase',
+    price: 15,
+    productImg: IMG_PATH.OTHER.GlassVase,
+  },
+  {
+    name: 'Hammershoi',
+    price: 45,
+    productImg: IMG_PATH.OTHER.Hammershoi,
+  },
+  {
+    name: 'Ceramic Vase',
+    price: 35,
+    productImg: IMG_PATH.OTHER.CeramicVase,
+  },
+  {
+    name: 'Steel vase',
+    price: 25,
+    productImg: IMG_PATH.OTHER.SteelVase,
+  },
+  {
+    name: 'Bamboo',
+    price: 15,
+    productImg: IMG_PATH.OTHER.Bamboo,
+  },
+  {
+    name: 'Rattan Grapefruit',
+    price: 48,
+    productImg: IMG_PATH.OTHER.RattanGrapefruit,
+  },
+  {
+    name: 'Lime & Matcha',
+    price: 46,
+    productImg: IMG_PATH.OTHER.LimeMatcha,
+  },
+  {
+    name: 'Cedar & Lavender',
+    price: 64,
+    productImg: IMG_PATH.OTHER.CedarLavender,
+  },
+  {
+    name: 'Ocean Mist',
+    price: 58,
+    productImg: IMG_PATH.OTHER.OceanMist,
+  },
+];
