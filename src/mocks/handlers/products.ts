27acf76dad8c44f935e00be50bb0f8809394d48e
woong <ckehfqkr302@naver.com>
2024-05-20 22:51:07 +0900

    url 변경

--- src/mocks/handlers/products.ts
+++ src/mocks/handlers/products.ts
@@ -1,8 +1,8 @@
 import { http, HttpResponse } from 'msw';
-import { products } from '../data/products';
+import { products } from '../data/freshflowers';
 
 export const productsHandlers = [
-  http.get('/products', () => {
+  http.get('/v1/freshflowers/products', () => {
     return HttpResponse.json({ products });
   }),
 ];
