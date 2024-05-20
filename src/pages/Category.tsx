619f80338523e61d03c919b53b8f1798983f5e92
woong <ckehfqkr302@naver.com>
2024-05-20 22:48:44 +0900

    파일명 변경

--- src/pages/Category.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useSearchParams } from 'react-router-dom';
-
-const Category = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  return <div></div>;
-};
-
-export default Category;
---
+++ src/pages/Categorys.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Category from 'src/components/category';
+
+const CategoryPage = () => {
+  return <Category />;
+};
+
+export default CategoryPage;
