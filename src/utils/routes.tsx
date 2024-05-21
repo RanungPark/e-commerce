fa5878d2b9f98927da1c98fa85a70492f4465bce
woong <ckehfqkr302@naver.com>
2024-05-21 15:07:41 +0900

    라우터 경로 변경

--- src/utils/routes.tsx
+++ src/utils/routes.tsx
@@ -1,9 +1,9 @@
 import { ReactNode } from 'react';
-import About from 'src/pages/About';
-import Checkout from 'src/pages/Checkout';
-import Category from 'src/pages/Category';
-import Home from 'src/pages/Home';
-import Products from 'src/pages/Products';
+import AboutPage from 'src/pages/About';
+import CategoryPage from 'src/pages/Categorys';
+import CheckoutPage from 'src/pages/Checkout';
+import HomePage from 'src/pages/Home';
+import ProductsPage from 'src/pages/Products';
 
 export interface Routes {
   path: string;
@@ -11,9 +11,12 @@ export interface Routes {
 }
 
 export const routes: Routes[] = [
-  { path: '', element: <Home /> },
-  { path: '/categories?name=:categoryName', element: <Category /> },
-  { path: '/products/:productId', element: <Products /> },
-  { path: '/company/about', element: <About /> },
-  { path: '/order/checkout', element: <Checkout /> },
+  { path: '', element: <HomePage /> },
+  { path: '/categories/:categoryName', element: <CategoryPage /> },
+  {
+    path: 'categories/:categoryName/products/:productId',
+    element: <ProductsPage />,
+  },
+  { path: '/company/about', element: <AboutPage /> },
+  { path: '/order/checkout', element: <CheckoutPage /> },
 ];
