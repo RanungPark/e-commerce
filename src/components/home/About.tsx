a27b029096fe6ee66e90828d007fe8dd1fac11cf
woong <ckehfqkr302@naver.com>
2024-05-22 21:52:30 +0900

    어바웃 컴포넌트

---
+++ src/components/home/About.tsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import styled from 'styled-components';
+import Text from '../common/Text';
+import { mixins } from 'src/styles/Mixin';
+import { PartialOptional } from 'src/@types/utils';
+import Buttons from '../common/Buttons';
+import { homeAboutDatas } from 'src/data/homeAbout';
+
+interface AboutTitleProps {
+  title: string;
+}
+
+interface AboutInfoProps extends AboutTitleProps {
+  info: string;
+  caption: string;
+  isButton: boolean;
+}
+
+type OptionalFormAboutInfoProps = PartialOptional<
+  AboutInfoProps,
+  'isButton' | 'caption'
+>;
+
+const About = () => {
+  return (
+    <div>
+      {homeAboutDatas.map(aboutData => (
+        <>
+          <AboutTitle title={aboutData.title} />
+          {aboutData.infos.map(infoData => (
+            <AboutInfo
+              title={infoData.title}
+              info={infoData.info}
+              caption={infoData?.caption}
+              isButton={infoData?.isButton}
+            />
+          ))}
+        </>
+      ))}
+    </div>
+  );
+};
+
+const AboutTitle = ({ title }: AboutTitleProps) => {
+  return (
+    <AboutTitleWrapper>
+      <Text as="h2" typography="Heading2">
+        {title}
+      </Text>
+    </AboutTitleWrapper>
+  );
+};
+
+const AboutInfo = ({
+  title,
+  info,
+  isButton = false,
+  caption = '',
+}: OptionalFormAboutInfoProps) => {
+  return (
+    <AboutInfoWrapper>
+      {caption ? (
+        <Text as="span" typography="Caption">
+          {caption}
+        </Text>
+      ) : (
+        <></>
+      )}
+      <Text as="h3" typography="Heading3">
+        {title}
+      </Text>
+      <Text as="p" typography="Body">
+        {info}
+      </Text>
+      {isButton ? (
+        <ButtonWrapper>
+          <Buttons
+            label="Lern more"
+            buttonType="contained"
+            colorType="secondary"
+          />
+        </ButtonWrapper>
+      ) : (
+        <></>
+      )}
+    </AboutInfoWrapper>
+  );
+};
+
+const AboutTitleWrapper = styled.article`
+  ${mixins.border({ width: 'leftAndBottom' })};
+  ${mixins.border({ width: 'right' })};
+  padding: 80px;
+`;
+
+const AboutInfoWrapper = styled(AboutTitleWrapper)`
+  & span {
+    display: inline-block;
+    margin-bottom: 24px;
+  }
+
+  & h3 {
+    margin-bottom: 16px;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  margin-top: 65px;
+
+  & button {
+    width: 56px;
+  }
+`;
+
+export default About;
