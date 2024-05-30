5f3838b4b8e382e05b4ddc65d2e62a63e2093b55
woong <ckehfqkr302@naver.com>
2024-05-30 22:21:44 +0900

    박스 모델 글로벌 스타일에 추가

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,10 +1,12 @@
-import { createGlobalStyle } from "styled-components";
-import reset from "./reset";
-import base from "./base";
+import { createGlobalStyle } from 'styled-components';
+import reset from './reset';
+import base from './base';
+import { box } from './box';
 
 const GlobalStyle = createGlobalStyle`
 ${reset}
 ${base}
+${box()}
 `;
 
 export default GlobalStyle;
--- src/styles/box.ts
+++ src/styles/box.ts
@@ -1,6 +1,6 @@
 import { css } from 'styled-components';
 
-function box() {
+export function box() {
   return Array(100)
     .fill(1)
     .reduce((prev, curr, currIndex) => {
@@ -13,6 +13,45 @@ function box() {
         }
         .mt-${index} {
           margin-top: ${index * size}px;
+        }
+        .mb-${index} {
+          margin-bottom: ${index * size}px;
+        }
+        .mr-${index} {
+          margin-right: ${index * size}px;
+        }
+        .ml-${index} {
+          margin-left: ${index * size}px;
+        }
+        .p-${index} {
+          padding: ${index * size}px;
+        }
+        .pt-${index} {
+          padding-top: ${index * size}px;
+        }
+        .pb-${index} {
+          padding-bottom: ${index * size}px;
+        }
+        .pr-${index} {
+          padding-right: ${index * size}px;
+        }
+        .pl-${index} {
+          padding-left: ${index * size}px;
+        }
+        .b-${index} {
+          border: ${index}px solid ${({ theme }) => theme.colors.black};
+        }
+        .bt-${index} {
+          border-top: ${index}px solid ${({ theme }) => theme.colors.black};
+        }
+        .bb-${index} {
+          border-bottom: ${index}px solid ${({ theme }) => theme.colors.black};
+        }
+        .br-${index} {
+          border-right: ${index}px solid ${({ theme }) => theme.colors.black};
+        }
+        .bl-${index} {
+          border-left: ${index}px solid ${({ theme }) => theme.colors.black};
         }
       `;
     }, '');
