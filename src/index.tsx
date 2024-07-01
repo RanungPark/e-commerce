78c479a26491551b60aa0d8e72bdf26a21a75565
woong <ckehfqkr302@naver.com>
2024-07-01 16:09:43 +0900

    리액트 쿼리 준비

--- src/index.tsx
+++ src/index.tsx
@@ -6,19 +6,27 @@ import { theme } from '@styles/theme';
 import GlobalStyle from '@styles/GlobalStyle';
 import router from './router';
 import { worker } from '@mocks/worker';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 if (process.env.NODE_ENV === 'development') {
   worker.start();
 }
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={true} />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
