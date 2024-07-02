99ecc83686b64f82c7016bd5f4323da1a0de4ec1
woong <ckehfqkr302@naver.com>
2024-07-02 19:22:33 +0900

    api 유틸 추가

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,3 +1,15 @@
-export function fetchCategory() {
-  return fetch(`/freshflowers/products`).then(response => response.json());
-}
+export const fetchCategory = async (categoryName: string) => {
+  const response = await fetch(`/${categoryName}`);
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
+
+export const fetchProduct = async (categoryName: string, id: string) => {
+  const response = await fetch(`/${categoryName}/${id}`);
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
