7f51c0a7cfb6e0d93388268d8ab4fdb865153d93
woong <ckehfqkr302@naver.com>
2024-07-02 19:22:44 +0900

    데이터 정보 수정

--- src/mocks/data/freshflowers.ts
+++ src/mocks/data/freshflowers.ts
@@ -1,80 +1,80 @@
-import { Products, Rep } from 'src/@types/product';
+import { ProductType, RepType } from 'src/@types/product';
 import { IMG_PATH } from '@constants/imgPath';
 
-export const freshflowersRep: Rep = {
+export const freshflowersRep: RepType = {
   type: 'Fresh Flowers',
   img: IMG_PATH.OTHER.FreshFlowers,
 };
 
-export const products: Products[] = [
+export const freshflowersProducts: ProductType[] = [
   {
     id: 1,
     name: 'Snowfall',
     price: 70,
     productImg: IMG_PATH.FLOWER.Snowfall,
-    info: 'The Snowfall bouquet features elegant white roses known for their delicate fragrance, paired with seasonal greenery to enhance their pure beauty. This exquisite arrangement adds sophistication and grace to any occasion.',
+    info: 'Snowfall 부케는 은은한 향기로 유명한 우아한 화이트 장미와 계절의 푸르름이 어우러져 순수한 아름다움을 더욱 돋보이게 합니다. 이 절묘한 부케는 어떤 상황에서도 세련미와 우아함을 더합니다.',
   },
   {
     id: 2,
     name: `Dawn's Delight`,
     price: 70,
     productImg: IMG_PATH.FLOWER.DawnsDelight,
-    info: `The Dawn's Delight bouquet showcases radiant peach and pink roses, renowned for their captivating hues and gentle scent. Accented with seasonal foliage, this arrangement brings warmth and charm to any setting.`,
+    info: `Dawn's Delight 부케는 매혹적인 색감과 은은한 향기로 유명한 복숭아와 핑크 장미를 선보입니다. 계절의 잎사귀로 포인트를 준 이 부케는 어떤 환경에서도 따뜻함과 매력을 선사합니다.`,
   },
   {
     id: 3,
     name: 'Pink Elegance',
     price: 70,
     productImg: IMG_PATH.FLOWER.PinkElegance,
-    info: 'The Pink Elegance bouquet features stunning pink roses, admired for their vibrant color and sweet fragrance. Paired with seasonal greenery, this arrangement exudes grace and sophistication.',
+    info: 'Pink Elegance 부케는 선명한 색상과 달콤한 향기로 사랑받는 아름다운 핑크 장미가 특징입니다. 계절의 푸르름과 어우러진 이 부케는 우아함과 세련미를 발산합니다.',
   },
   {
     id: 4,
     name: 'Rustic Charm',
     price: 70,
     productImg: IMG_PATH.FLOWER.RusticCharm,
-    info: 'The Rustic Charm bouquet combines earthy-toned roses with wildflowers, creating a natural and charming aesthetic. Enhanced with seasonal foliage, this arrangement evokes a sense of countryside beauty.',
+    info: 'Rustic Charm 부케는 흙빛 장미와 야생화를 결합하여 자연스럽고 매력적인 아름다움을 연출합니다. 계절의 단풍으로 더욱 돋보이는 이 부케는 시골의 아름다움을 떠올리게 합니다.',
   },
   {
     id: 5,
     name: 'Autumn Symphony',
     price: 70,
     productImg: IMG_PATH.FLOWER.AutumnSymphony,
-    info: 'The Autumn Symphony bouquet features a harmonious blend of warm-toned roses and seasonal foliage, capturing the essence of fall. This arrangement brings the vibrant colors and cozy feel of autumn indoors.',
+    info: 'Autumn Symphony 부케는 따뜻한 톤의 장미와 제철 단풍이 조화롭게 어우러져 가을의 정수를 담아낸 것이 특징입니다. 가을의 생생한 색감과 아늑한 느낌을 실내에서도 느낄 수 있는 부케입니다.',
   },
   {
     id: 6,
     name: 'Rosy Delight',
     price: 70,
     productImg: IMG_PATH.FLOWER.RosyDelight,
-    info: 'The Rosy Delight bouquet features a selection of David Austin roses, celebrated for their beauty and subtle fragrance. Complemented by seasonal foliage, this arrangement enhances the sublime charm of these exquisite flowers.',
+    info: 'Rosy Delight 부케는 아름다움과 은은한 향기로 유명한 데이비드 오스틴 장미를 엄선하여 선보입니다. 제철 잎사귀와 어우러져 이 아름다운 꽃의 숭고한 매력을 더욱 돋보이게 하는 부케입니다.',
   },
   {
     id: 7,
     name: 'Serenity',
     price: 89,
     productImg: IMG_PATH.FLOWER.Serenity,
-    info: 'Large exceptional bouquet composed of a selection of David Austin roses, known for their beauty and subtle fragrance. The bouquet is accompanied by seasonal foliage which will enhance these sublime flowers even',
+    info: 'Serenity 부케는 평화롭고 차분한 느낌을 주는 흰색과 파란색 꽃이 특징입니다. 섬세한 계절의 녹색과 어우러져 어떤 공간에도 평온한 느낌을 선사합니다.',
   },
   {
     id: 8,
     name: 'Blue Harmony',
     price: 55,
     productImg: IMG_PATH.FLOWER.BlueHarmony,
-    info: 'The Serenity bouquet features calming white and blue flowers, known for their peaceful and soothing presence. Paired with delicate seasonal greenery, this arrangement brings a sense of tranquility to any space.',
+    info: 'Blue Harmony 부케는 차분한 느낌을 주는 블루 톤의 꽃을 엄선한 것이 특징입니다. 계절의 초록색과 어우러져 어디에 전시하든 고요한 분위기를 연출합니다.',
   },
   {
     id: 9,
     name: 'Mystical Majesty',
     price: 80,
     productImg: IMG_PATH.FLOWER.MysticalMajesty,
-    info: 'The Mystical Majesty bouquet features deep purple and blue flowers, celebrated for their rich hues and enchanting allure. Accented with lush seasonal foliage, this arrangement exudes an air of mystery and elegance.',
+    info: 'Mystical Majesty 부케는 풍부한 색감과 매혹적인 매력으로 유명한 짙은 보라색과 파란색 꽃이 특징입니다. 계절에 따라 무성한 잎사귀로 포인트를 준 이 부케는 신비롭고 우아한 분위기를 자아냅니다.',
   },
   {
     id: 10,
     name: 'Blazing Blossoms',
     price: 70,
     productImg: IMG_PATH.FLOWER.BlazingBlassoms,
-    info: 'The Blazing Blossoms bouquet showcases vibrant red and orange flowers, known for their fiery colors and bold presence. Paired with seasonal greenery, this arrangement brings energy and warmth to any setting.',
+    info: 'Blazing Blossoms 부케는 강렬한 붉은색과 오렌지색 꽃이 돋보이는 부케로, 불 같은 색감과 대담한 존재감이 특징입니다. 계절의 푸르름과 어우러진 이 부케는 어떤 환경에서도 에너지와 따뜻함을 선사합니다.',
   },
 ];
