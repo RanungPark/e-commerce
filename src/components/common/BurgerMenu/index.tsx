8b1726c7b7808c31d20aff6fa9335851d63822d8
woong <ckehfqkr302@naver.com>
2024-07-04 22:07:09 +0900

    burger 컴포넌트

--- src/components/common/BurgerMenu/index.tsx
+++ src/components/common/BurgerMenu/index.tsx
@@ -2,27 +2,90 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Text from '../Text';
+import { zIndex } from '@constants/zIndex';
+import { burgerMenus } from '@data/bugerMenu';
+import { ReactComponent as WClose } from '@assets/icons/wght400/WClose.svg';
+import { mixins } from '@styles/Mixin';
+import { IconInfos } from '../ContactIcons';
 
-const BurgerMenu = () => {
+type BurgerMenuProps = {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const BurgerMenu = ({ open, setOpen }: BurgerMenuProps) => {
+  const handleBurgerMenuClose = () => {
+    setOpen(!open);
+  };
+
   return (
-    <StyledBugerMenu>
-      <Menu to={'/'}>
-        <Text as="h5" typography="Heading5">
-          {'Sign in'}
-        </Text>
-      </Menu>
-    </StyledBugerMenu>
+    <>
+      <BugerMenuBackGround />
+      <StyledBugerMenu className="bb-1 br-1 bl-1">
+        <CloseButton className="p-2" onClick={handleBurgerMenuClose}>
+          <WClose />
+        </CloseButton>
+        {burgerMenus.map(menu => (
+          <Menu
+            key={menu.content}
+            to={menu.link}
+            className="bt-1 p-3"
+            onClick={handleBurgerMenuClose}
+          >
+            <Text as="h5" typography="Heading5">
+              {menu.content}
+            </Text>
+          </Menu>
+        ))}
+        <InfoWrapper className="bt-1 p-3">
+          {['Shipping & returns', 'Terms & conditions', 'Privacy policy'].map(
+            info => (
+              <Text key={info} as="p" typography="Links">
+                {info}
+              </Text>
+            )
+          )}
+        </InfoWrapper>
+        <IconsWrapper className="bt-1 p-3">
+          <IconInfos />
+        </IconsWrapper>
+      </StyledBugerMenu>
+    </>
   );
 };
 
+const BugerMenuBackGround = styled.div`
+  width: 768px;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.2);
+  position: fixed;
+  z-index: ${zIndex.bugerMenuBackGround};
+`;
+
 const StyledBugerMenu = styled.div`
-  width: 100%;
-  position: absolute;
-  background-color: #000000;
+  width: 385px;
+  background-color: ${({ theme }) => theme.colors.white};
+  height: 100vh;
+  position: fixed;
+  z-index: ${zIndex.bugerMenu};
 `;
 
 const Menu = styled(Link)`
-  margin: 24px;
+  display: block;
+`;
+
+const CloseButton = styled.button`
+  ${mixins.flexBox({ justify: 'start' })}
+  width: 100%;
+  cursor: pointer;
+`;
+
+const IconsWrapper = styled.div``;
+
+const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
 `;
 
 export default BurgerMenu;
