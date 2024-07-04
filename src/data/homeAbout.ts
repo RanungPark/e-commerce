6f38fe8d73b332fb16ce2311ddb4117a588ac6aa
woong <ckehfqkr302@naver.com>
2024-07-04 22:10:37 +0900

    home about 데이터 수정

--- src/data/homeAbout.ts
+++ src/data/homeAbout.ts
@@ -23,7 +23,7 @@ export const homeAboutDatas: HomeAboutData[] = [
     infos: [
       {
         title: 'Kyiv LuxeBouquets',
-        info: `We are a modern local floral studio, which specializes in the design and delivery of unique bouquets. We have the best florists who carefully select each look, our studio cooperates directly with farms for growing different flowers, so we always have fresh flowers, which are collected by our florists in exquisite bouquets. We have a collection of fresh bouquets, collections of dried bouquets, house plants, as well as fragrant candles from luxury brands to create the perfect atmosphere. Make someone's day amazing by sending flowers, plants and gifts the same or next day. Ordering flowers online has never been easier.`,
+        info: `저희는 독특한 꽃다발을 전문적으로 디자인하고 배달하는 현대적인 로컬 플로럴 스튜디오입니다. 저희 스튜디오에는 각 룩을 신중하게 선택하는 최고의 플로리스트가 있으며, 다양한 꽃을 재배하기 위해 농장과 직접 협력하므로 항상 신선한 꽃을 보유하고 있으며 플로리스트가 정교한 꽃다발로 수집합니다. 생화 컬렉션, 드라이 부케 컬렉션, 관엽 식물, 럭셔리 브랜드의 향기로운 캔들까지 완벽한 분위기를 연출할 수 있는 다양한 컬렉션을 갖추고 있습니다. 꽃, 식물, 선물을 당일 또는 익일 배송으로 누군가의 하루를 특별하게 만들어 보세요. 온라인으로 꽃을 주문하는 것이 그 어느 때보다 쉬워졌습니다.`,
         isButton: true,
         caption: 'our story',
       },
@@ -34,19 +34,19 @@ export const homeAboutDatas: HomeAboutData[] = [
     infos: [
       {
         title: 'Stylish bouquets by florists',
-        info: `At our floral studio, our professional florists craft the most elegant and stylish bouquets using only the freshest and highest quality materials available. We stay up-to-date with the latest floral design trends and offer unique arrangements that are sure to impress. Let us brighten up your day with our stunning bouquets and same-day delivery service.`,
+        info: `플로리스트 스튜디오에서는 전문 플로리스트가 가장 신선하고 최고 품질의 재료만을 사용하여 가장 우아하고 세련된 부케를 제작합니다. 저희는 최신 플로럴 디자인 트렌드를 파악하고 있으며 감동을 선사하는 독특한 어레인지먼트를 제공합니다. 멋진 부케와 당일 배송 서비스로 여러분의 하루를 더욱 빛나게 해드리겠습니다.`,
       },
       {
         title: 'On-time delivery',
-        info: `Never miss a moment with our on-time flower delivery service. Our couriers will deliver your bouquet personally, without boxes, to ensure it arrives in perfect condition. Trust us to deliver your thoughtful gift reliably.`,
+        info: `정시 꽃 배달 서비스로 한순간도 놓치지 마세요. 택배 기사가 박스 없이 직접 꽃다발을 배달하여 완벽한 상태로 도착할 수 있도록 합니다. 정성스러운 선물을 안정적으로 배달해 드립니다.`,
       },
       {
         title: 'Safe payment',
-        info: `You can feel secure when placing an order with us, as we use industry-standard security measures to protect your payment information. Your transaction will be safe and hassle-free, so you can shop with confidence.`,
+        info: `결제 정보를 보호하기 위해 업계 표준 보안 조치를 사용하므로 안심하고 주문할 수 있습니다. 거래가 안전하고 번거롭지 않으므로 안심하고 쇼핑할 수 있습니다.`,
       },
       {
         title: 'Subscription by your needs',
-        info: `With our subscription service tailored to your specific needs, you can enjoy the convenience of having beautiful bouquets delivered straight to your door at regular intervals. Our flexible service is perfect for busy individuals or those who want to ensure they always have fresh flowers on hand. You'll save time and money with this hassle-free solution to your floral needs.`,
+        info: `특정 요구사항에 맞춘 구독 서비스를 이용하면 아름다운 꽃다발을 정기적으로 집 앞까지 배달해 주는 편리함을 누릴 수 있습니다. 이 유연한 서비스는 바쁜 개인이나 항상 신선한 꽃을 구비하고 싶은 분들에게 적합합니다. 번거로움 없이 꽃을 받아볼 수 있는 이 서비스를 이용하면 시간과 비용을 절약할 수 있습니다.`,
       },
     ],
   },
