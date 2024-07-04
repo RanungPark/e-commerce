be29bac51a5da9c7b61af165d5dfc48bd74e64a8
woong <ckehfqkr302@naver.com>
2024-07-04 22:08:45 +0900

    burger 데이터

---
+++ src/data/bugerMenu.ts
@@ -0,0 +1,27 @@
+type burgerMenuType = {
+  link: string;
+  content: string;
+};
+
+export const burgerMenus: burgerMenuType[] = [
+  {
+    link: '/login',
+    content: 'Sign in',
+  },
+  {
+    link: '/',
+    content: 'Shop',
+  },
+  {
+    link: '/servise',
+    content: 'Servise',
+  },
+  {
+    link: '/contact',
+    content: 'Contact',
+  },
+  {
+    link: '/about',
+    content: 'About us',
+  },
+];
