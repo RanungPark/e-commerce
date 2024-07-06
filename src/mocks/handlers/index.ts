c196210517c1a3c838e5bb7f2fc6b479890f7fe1
woong <ckehfqkr302@naver.com>
2024-07-06 17:05:18 +0900

    인증 핸들러 추가

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,4 +1,9 @@
+import { authorizationHandlers } from './authorization';
 import { categoryHandlers } from './categorys';
 import { productHandlers } from './products';
 
-export const handlers = [...categoryHandlers, ...productHandlers];
+export const handlers = [
+  ...categoryHandlers,
+  ...productHandlers,
+  ...authorizationHandlers,
+];
