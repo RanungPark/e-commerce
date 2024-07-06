36a9a379bad29ef3b419076c1ad17f4fcbdeaaa6
woong <ckehfqkr302@naver.com>
2024-07-06 17:04:56 +0900

    helmet 제거

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import Home from '@components/home';
 import About from '@components/home/About';
-import { Helmet } from 'react-helmet';
 
 const HomePage = () => {
   return (
     <>
-      <Helmet>
-        <title>오늘의 꽃</title>
-      </Helmet>
       <Home />
       <About />
     </>
