3ab83b04651beb8b95050cd34c10967a3bb9f4a1
woong <ckehfqkr302@naver.com>
2024-07-06 17:05:30 +0900

    인증 api 추가

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -13,3 +13,38 @@ export const fetchProduct = async (categoryName: string, id: string) => {
   }
   return response.json();
 };
+
+export const fetchJoin = async () => {
+  const response = await fetch(`/join`);
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  return response.json();
+};
+
+export const fetchLogin = async (username: string) => {
+  const response = await fetch('/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username }),
+  });
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
+
+export const fetchSignUp = async () => {
+  const response = await fetch(`/signup`);
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  return response.json();
+};
