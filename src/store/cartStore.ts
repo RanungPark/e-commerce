0a35781e701c24aa5eca5dc5fa93a16a12ee8b8c
woong <ckehfqkr302@naver.com>
2024-07-19 20:02:39 +0900

    devtool 추가와 이벤트 수정

--- src/store/cartStore.ts
+++ src/store/cartStore.ts
@@ -1,38 +1,51 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 
 export interface CartItem {
   id: number;
   name: string;
+  productImg: string;
   price: number;
   quantity: number;
 }
 
 export interface CartState {
   cart: CartItem[];
-  addItem: (item: Omit<CartItem, 'quantity'>) => void;
+  addItem: (item: CartItem) => void;
   removeItem: (id: number) => void;
   clearCart: () => void;
 }
 
-export const useCartStore = create<CartState>(set => ({
-  cart: [],
-  addItem: item =>
-    set(state => {
-      const existingItem = state.cart.find(cartItem => cartItem.id === item.id);
-      if (existingItem) {
-        return {
-          cart: state.cart.map(cartItem =>
-            cartItem.id === item.id
-              ? { ...cartItem, quantity: cartItem.quantity + 1 }
-              : cartItem
-          ),
-        };
-      }
-      return { cart: [...state.cart, { ...item, quantity: 1 }] };
+export const useCartStore = create<CartState>()(
+  devtools(
+    set => ({
+      cart: [],
+      addItem: item =>
+        set(state => {
+          const existingItem = state.cart.find(
+            cartItem => cartItem.id === item.id
+          );
+          if (existingItem) {
+            return {
+              cart: state.cart.map(cartItem =>
+                cartItem.quantity !== item.quantity
+                  ? {
+                      ...cartItem,
+                      quantity: item.quantity,
+                      price: item.price,
+                    }
+                  : cartItem
+              ),
+            };
+          }
+          return { cart: [...state.cart, { ...item }] };
+        }),
+      removeItem: id =>
+        set(state => ({
+          cart: state.cart.filter(item => item.id !== id),
+        })),
+      clearCart: () => set({ cart: [] }),
     }),
-  removeItem: id =>
-    set(state => ({
-      cart: state.cart.filter(item => item.id !== id),
-    })),
-  clearCart: () => set({ cart: [] }),
-}));
+    { name: 'cartStore' }
+  )
+);
