3466d872732d4f242f2a050f758450551b95367a
woong <ckehfqkr302@naver.com>
2024-07-20 16:41:55 +0900

    라우터 수정

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -3,9 +3,8 @@ import CategoryPage from './CategoryPage';
 import HomePage from './HomePage';
 import ProductsPage from './ProductsPage';
 import { ReactNode } from 'react';
-import OrderPage from './OrderPage';
 import LoginPage from './LoginPage';
-import SingupPage from './SingupPage';
+import CheckoutPage from './CheckoutPage';
 
 export interface Routes {
   path: string;
@@ -15,12 +14,11 @@ export interface Routes {
 export const routes: Routes[] = [
   { path: '', element: <HomePage /> },
   { path: '/login', element: <LoginPage /> },
-  { path: '/signup', element: <SingupPage /> },
   { path: '/categories/:categoryName', element: <CategoryPage /> },
   {
     path: 'categories/:categoryName/products/:productId',
     element: <ProductsPage />,
   },
   { path: '/company/about', element: <AboutPage /> },
-  { path: '/order', element: <OrderPage /> },
+  { path: '/checkout', element: <CheckoutPage /> },
 ];
