067cf59b82c01bb1963d1bb11b1fbfeae3a3e8ff
woong <ckehfqkr302@naver.com>
2024-07-21 20:24:05 +0900

    정규표현식 상수

---
+++ src/constants/reg.ts
@@ -0,0 +1,7 @@
+export const REG_NAME = /^[가-힣]+$|^[a-zA-Z\s]+$/;
+export const REG_EMAIL = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+export const REG_PHONE = /^010\d{8}$/;
+export const REG_STRING = /^[a-zA-Z가-힣]+$/;
+export const REG_NUMBER = /^\d+$/;
+export const REG_STRING_NUMBER = /^[a-zA-Z가-힣0-9]+$/;
+export const REG_DATE = /^(0[1-9]|1[0-2])\/\d{2}$/;
