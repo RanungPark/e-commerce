c88b3f16efc33c87aa109719129b7b8d481c3ff1
woong <ckehfqkr302@naver.com>
2024-07-30 20:30:48 +0900

    파일명 변경

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { RouterProvider } from 'react-router-dom';
 import { theme } from '@styles/theme';
-import GlobalStyle from '@styles/GlobalStyle';
+import GlobalStyles from '@styles/GlobalStyles';
 import router from './router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { worker } from '@mocks/browser';
@@ -25,7 +25,7 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
-        <GlobalStyle />
+        <GlobalStyles />
         <RouterProvider router={router} />
       </ThemeProvider>
     </QueryClientProvider>
