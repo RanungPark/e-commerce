0bc07c11012def4abf7a2d316ce884be089f41eb
woong <ckehfqkr302@naver.com>
2024-07-31 20:11:55 +0900

    아이콘버튼 컴포넌트 및 스토리북

---
+++ src/components/buttons/IconButton.tsx
@@ -0,0 +1,41 @@
+import styled, { css } from 'styled-components';
+
+type IconButtonSize = 'medium' | 'large';
+
+interface IconButtonProps {
+  size?: IconButtonSize;
+  IconComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  onClick?: () => void;
+}
+
+interface IconButtonWrapperProps {
+  size: IconButtonSize;
+}
+
+const IconButton = ({
+  size = 'medium',
+  IconComponent,
+  onClick,
+}: IconButtonProps) => {
+  return (
+    <IconButtonWrapper size={size} onClick={onClick}>
+      <IconComponent />
+    </IconButtonWrapper>
+  );
+};
+
+const mediumStyle = css`
+  width: 24px;
+  height: 24px;
+`;
+
+const largeStyle = css`
+  width: 44px;
+  height: 44px;
+`;
+
+const IconButtonWrapper = styled.button<IconButtonWrapperProps>`
+  ${({ size }) => (size === 'medium' ? mediumStyle : largeStyle)}
+`;
+
+export default IconButton;
