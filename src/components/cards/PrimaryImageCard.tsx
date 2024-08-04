b4e6fb429a9842a1554f6db4a5ff7fa4638e0284
woong <ckehfqkr302@naver.com>
2024-08-04 22:34:37 +0900

    프라이머리 이미지 카드 컴포넌트 및 스토리북

---
+++ src/components/cards/PrimaryImageCard.tsx
@@ -0,0 +1,59 @@
+import { mixins } from '@styles/Mixin';
+import styled from 'styled-components';
+
+interface PrimaryImageCardProps {
+  imgPath: string;
+  alt: string;
+  price: number;
+  children: string;
+  onClick: (e: React.MouseEvent) => void;
+}
+
+const PrimaryImageCard = ({
+  imgPath,
+  alt,
+  price,
+  children,
+  onClick,
+}: PrimaryImageCardProps) => {
+  return (
+    <PrimaryImageCardWrapper className="bl-1 bb-1" onClick={onClick}>
+      <PrimaryImageCardImg alt={alt} src={imgPath} />
+      <PrimaryImageCardTextWrapper>
+        {children}
+        <PrimaryImageCardPriceWrapper>
+          price {price}$
+        </PrimaryImageCardPriceWrapper>
+      </PrimaryImageCardTextWrapper>
+    </PrimaryImageCardWrapper>
+  );
+};
+
+const PrimaryImageCardWrapper = styled.section`
+  ${mixins.flexBox({})}
+  width: 100%;
+  height: ${768 / 2}px;
+  position: relative;
+  cursor: pointer;
+`;
+
+const PrimaryImageCardImg = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+const PrimaryImageCardTextWrapper = styled.div`
+  position: absolute;
+  ${mixins.flexBox({ direction: 'column' })}
+  ${({ theme }) => theme.typography.Heading6}
+  background-color: transparent;
+  gap: 4px;
+  bottom: 24px;
+`;
+
+const PrimaryImageCardPriceWrapper = styled.p`
+  ${({ theme }) => theme.typography.Caption}
+  color: ${({ theme }) => theme.colors.gray};
+`;
+
+export default PrimaryImageCard;
---
+++ src/stories/cards/PrimaryImageCard.stories.ts
@@ -0,0 +1,51 @@
+import PrimaryImageCard from '@components/cards/PrimaryImageCard';
+import { IMG_PATH } from '@constants/imgPath';
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+const meta = {
+  title: 'Cards/PrimaryImageCard',
+  component: PrimaryImageCard,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    imgPath: {
+      control: 'text',
+      description: 'PrimaryImageCard 이미지 경로',
+      defaultValue: IMG_PATH.FLOWER.BlazingBlassoms,
+    },
+    alt: {
+      control: 'text',
+      description: 'PrimaryImageCard 이미지 대체 속성',
+      defaultValue: 'Bouquets Fresh Flowers',
+    },
+    price: {
+      control: 'text',
+      description: 'PrimaryImageCard 가격',
+      defaultValue: 70,
+    },
+    children: {
+      control: 'text',
+      description: 'PrimaryImageCard 타이틀',
+      defaultValue: 'Bouquets Fresh Flowers',
+    },
+    onClick: {
+      description: 'PrimaryImageCard 클릭 이벤트',
+    },
+  },
+  args: { onClick: fn() },
+} satisfies Meta<typeof PrimaryImageCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    imgPath: IMG_PATH.FLOWER.BlazingBlassoms,
+    alt: 'Bouquets Fresh Flowers',
+    price: 70,
+    children: 'Bouquets Fresh Flowers',
+  },
+};
