c1d50d13478683099d5be94c3cbb1eb652f5e2d3
woong <ckehfqkr302@naver.com>
2024-08-04 22:32:39 +0900

    텍스트 카드 컴포넌트 및 스토리북

---
+++ src/stories/cards/TextCard.stories.tsx
@@ -0,0 +1,44 @@
+import TextCard from '@components/cards/TextCard';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Cards/TextCard',
+  component: TextCard,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  decorators: [
+    Story => (
+      <div style={{ width: '728px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+  argTypes: {
+    title: {
+      control: 'text',
+      description: 'TextCard 제목',
+      defaultValue: 'Stylish bouquets by florists',
+    },
+    children: {
+      control: 'text',
+      description: 'TextCard 내용',
+      defaultValue: `The task of the organization, especially the strengthening and
+        development of the structure allows us to assess the importance of the
+        directions of progressive development.`,
+    },
+  },
+} satisfies Meta<typeof TextCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    title: 'Stylish bouquets by florists',
+    children: `The task of the organization, especially the strengthening and
+        development of the structure allows us to assess the importance of the
+        directions of progressive development.`,
+  },
+};
