def70741428bcc0d625c52f83d39eeff5b0d0208
woong <ckehfqkr302@naver.com>
2024-08-06 17:31:01 +0900

    파일 위치 변경

---
+++ src/stories/texts/TextFieldLabel.stories.ts
@@ -0,0 +1,28 @@
+import TextFieldLabel from '@components/texts/TextFieldLabel';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Texts/TextFieldLabel',
+  component: TextFieldLabel,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    children: {
+      control: 'text',
+      description: 'TextFieldLabel 텍스트',
+      defaultValue: 'Label',
+    },
+  },
+} satisfies Meta<typeof TextFieldLabel>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    htmlFor: '',
+    children: 'Label',
+  },
+};
