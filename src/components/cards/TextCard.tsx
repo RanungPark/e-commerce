c465d2c64f8ee4297656673867d37c621fa11716
woong <ckehfqkr302@naver.com>
2024-08-06 17:31:32 +0900

    텍스트 카드 라벨 추가

--- src/components/cards/TextCard.tsx
+++ src/components/cards/TextCard.tsx
@@ -3,11 +3,17 @@ import styled from 'styled-components';
 interface TextCardProps {
   title: string;
   children: string;
+  label?: string;
 }
 
-const TextCard = ({ title, children }: TextCardProps) => {
+const TextCard = ({ title, children, label = '' }: TextCardProps) => {
   return (
     <TextCardWrapper className="bb-1 bl-1 p-10">
+      {label ? (
+        <TextCardLabelWrapper className="mb-3">{label}</TextCardLabelWrapper>
+      ) : (
+        <></>
+      )}
       <TextCardTitle>{title}</TextCardTitle>
       {children}
     </TextCardWrapper>
@@ -22,6 +28,10 @@ const TextCardWrapper = styled.section`
   & h3 {
     margin-bottom: 16px;
   }
+`;
+
+const TextCardLabelWrapper = styled.p`
+  ${({ theme }) => theme.typography.Caption}
 `;
 
 const TextCardTitle = styled.h3`
