36e3459c505aa28d58562482a304180f65fe2e06
woong <ckehfqkr302@naver.com>
2024-08-07 10:44:27 +0900

    홈 페이지

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,40 @@
 import React from 'react';
-import Home from '@components/home';
-import About from '@components/home/About';
+import { v4 as uuidv4 } from 'uuid';
+import ActionCardAndImageCardList from '@components/lists/ActionCardAndImageCardList';
+import { useNavigate } from 'react-router';
+import HomeMain from '@components/fragments/HomeMain';
+import { shopDatas } from '@data/shopDatas';
+import HomeAbout from '@components/fragments/HomeAbout';
+import styled from 'styled-components';
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleShopClick = (url: string) => (e: React.MouseEvent) => {
+    e.preventDefault();
+    navigate(url);
+  };
+
   return (
-    <>
-      <Home />
-      <About />
-    </>
+    <HomePageWrapper className="br-1">
+      <HomeMain />
+      {shopDatas.map(({ children, imgPath, url }, index) => (
+        <ActionCardAndImageCardList
+          key={uuidv4()}
+          onClick={handleShopClick(url)}
+          index={index}
+          buttonText="Shop now"
+          imgPath={imgPath}
+          alt={children}
+        >
+          {children.replaceAll(' ', '')}
+        </ActionCardAndImageCardList>
+      ))}
+      <HomeAbout />
+    </HomePageWrapper>
   );
 };
+
+const HomePageWrapper = styled.main``;
 
 export default HomePage;
