73bad54312f29cfe2dc3234042160a9faf51969f
woong <ckehfqkr302@naver.com>
2024-08-09 18:18:41 +0900

    로그인 input 컴포넌트

---
+++ src/components/fragments/LoginPassword.tsx
@@ -0,0 +1,102 @@
+import PrimaryButton from '@components/buttons/PrimaryButton';
+import CompleteTextFiled from '@components/textfields/CompleteTextFiled';
+import DefaultTextField from '@components/textfields/DefaultTextField';
+import { loginDone } from '@constants/toast';
+import { loginStateType } from '@pages/LoginPage';
+import { useUserStore } from '@store/userStore';
+import { mixins } from '@styles/Mixin';
+import { useMutation } from '@tanstack/react-query';
+import { fetchJoin } from '@utils/api';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router';
+import { FormStateType } from 'src/@types/state';
+import styled from 'styled-components';
+
+interface LoginPasswordProps {
+  loginState: FormStateType;
+  onSubmit: (e: loginStateType) => void;
+  username: string;
+  loginCurrState: loginStateType;
+}
+
+const LoginPassword = ({
+  loginState,
+  onSubmit,
+  username,
+  loginCurrState,
+}: LoginPasswordProps) => {
+  const [join, setJoin] = useState(true);
+  const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
+  const { login } = useUserStore();
+
+  const type = loginCurrState;
+
+  const LoginPasswordMutation = useMutation({
+    mutationFn: (password: string) => fetchJoin(username, password, type),
+    onSuccess: data => {
+      const loginUser = {
+        id: data.id,
+        username: data.username,
+      };
+      onSubmit('done');
+      login(loginUser);
+      setJoin(true);
+      navigate(-1);
+      loginDone();
+    },
+    onError: error => {
+      setJoin(false);
+      console.error('Login failed:', error);
+    },
+  });
+
+  const onJoinValid = (data: any) => {
+    const { password } = data;
+    LoginPasswordMutation.mutate(password);
+  };
+
+  return (
+    <LoginPasswordWrapper>
+      {loginState === 'yet' && (
+        <CompleteTextFiled disabled={true}>
+          비밀번호를 입력해주세요.
+        </CompleteTextFiled>
+      )}
+
+      {loginState === 'curr' && (
+        <LoginPasswordForm onSubmit={handleSubmit(onJoinValid)}>
+          <DefaultTextField
+            inputState={join ? undefined : 'error'}
+            hasLabel={true}
+            label="비밀번호를 입렵해주세요"
+            hasHelpMessage={true}
+            helpMessage={join ? undefined : '로그인을 실패 하였습니다.'}
+          >
+            <input
+              {...register('password', {
+                required: '비밀번호를 입력해주세요',
+              })}
+              placeholder="비밀번호를 입력해주세요"
+              type="password"
+            />
+          </DefaultTextField>
+          <PrimaryButton onClick={handleSubmit(onJoinValid)}>
+            continue
+          </PrimaryButton>
+        </LoginPasswordForm>
+      )}
+    </LoginPasswordWrapper>
+  );
+};
+
+const LoginPasswordWrapper = styled.div``;
+
+const LoginPasswordForm = styled.form`
+  ${mixins.flexBox({ direction: 'column', align: 'start' })}
+  gap: 32px;
+  width: 100%;
+`;
+
+export default LoginPassword;
