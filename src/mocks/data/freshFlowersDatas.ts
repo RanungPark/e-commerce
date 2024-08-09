c3ee8f1bdeb827ba6790eab9acb94709153d818d
woong <ckehfqkr302@naver.com>
2024-08-09 13:29:40 +0900

    데이터 파일명 변경 및 상품 명 변경

--- src/mocks/data/freshFlowersDatas.ts
+++ src/mocks/data/freshFlowersDatas.ts
@@ -13,6 +13,7 @@ export const freshFlowersProducts: ProductType[] = [
     price: 70,
     imgPath: IMG_PATH.FreshFlowers.Snowfall,
     info: `Snowfall 생화는 은은한 향기로 유명한 우아한 화이트 장미와 계절의 푸르름이 어우러져 순수한 아름다움을 더욱 돋보이게 합니다. 이 절묘한 생화는 어떤 상황에서도 세련미와 우아함을 더합니다.`,
+    category: 'fresh flowers',
   },
   {
     id: 2,
@@ -20,6 +21,7 @@ export const freshFlowersProducts: ProductType[] = [
     price: 70,
     imgPath: IMG_PATH.FreshFlowers.DawnsDelight,
     info: `Dawn's Delight 생화는 매혹적인 색감과 은은한 향기로 유명한 복숭아와 핑크 장미를 선보입니다. 계절의 잎사귀로 포인트를 준 이 생화는 어떤 환경에서도 따뜻함과 매력을 선사합니다.`,
+    category: 'fresh flowers',
   },
   {
     id: 3,
@@ -27,6 +29,7 @@ export const freshFlowersProducts: ProductType[] = [
     price: 70,
     imgPath: IMG_PATH.FreshFlowers.PinkElegance,
     info: `Pink Elegance 생화는 선명한 색상과 달콤한 향기로 사랑받는 아름다운 핑크 장미가 특징입니다. 계절의 푸르름과 어우러진 이 생화는 우아함과 세련미를 발산합니다.`,
+    category: 'fresh flowers',
   },
   {
     id: 4,
@@ -34,6 +37,7 @@ export const freshFlowersProducts: ProductType[] = [
     price: 70,
     imgPath: IMG_PATH.FreshFlowers.RusticCharm,
     info: `Rustic Charm 생화는 흙빛 장미와 야생화를 결합하여 자연스럽고 매력적인 아름다움을 연출합니다. 계절의 단풍으로 더욱 돋보이는 이 생화는 시골의 아름다움을 떠올리게 합니다.`,
+    category: 'fresh flowers',
   },
   {
     id: 5,
@@ -41,6 +45,7 @@ export const freshFlowersProducts: ProductType[] = [
     price: 70,
     imgPath: IMG_PATH.FreshFlowers.AutumnSymphony,
     info: `Autumn Symphony 생화는 따뜻한 톤의 장미와 제철 단풍이 조화롭게 어우러져 가을의 정수를 담아낸 것이 특징입니다. 가을의 생생한 색감과 아늑한 느낌을 실내에서도 느낄 수 있는 생화입니다.`,
+    category: 'fresh flowers',
   },
   {
     id: 6,
@@ -48,6 +53,7 @@ export const freshFlowersProducts: ProductType[] = [
     price: 70,
     imgPath: IMG_PATH.FreshFlowers.RosyDelight,
     info: `Rosy Delight 생화는 아름다움과 은은한 향기로 유명한 데이비드 오스틴 장미를 엄선하여 선보입니다. 제철 잎사귀와 어우러져 이 아름다운 꽃의 숭고한 매력을 더욱 돋보이게 하는 생화입니다.`,
+    category: 'fresh flowers',
   },
   {
     id: 7,
@@ -55,6 +61,7 @@ export const freshFlowersProducts: ProductType[] = [
     price: 89,
     imgPath: IMG_PATH.FreshFlowers.Serenity,
     info: `Serenity 생화는 평화롭고 차분한 느낌을 주는 흰색과 파란색 꽃이 특징입니다. 섬세한 계절의 녹색과 어우러져 어떤 공간에도 평온한 느낌을 선사합니다.`,
+    category: 'fresh flowers',
   },
   {
     id: 8,
@@ -62,6 +69,7 @@ export const freshFlowersProducts: ProductType[] = [
     price: 55,
     imgPath: IMG_PATH.FreshFlowers.BlueHarmony,
     info: `Blue Harmony 생화는 차분한 느낌을 주는 블루 톤의 꽃을 엄선한 것이 특징입니다. 계절의 초록색과 어우러져 어디에 전시하든 고요한 분위기를 연출합니다.`,
+    category: 'fresh flowers',
   },
   {
     id: 9,
@@ -69,6 +77,7 @@ export const freshFlowersProducts: ProductType[] = [
     price: 80,
     imgPath: IMG_PATH.FreshFlowers.MysticalMajesty,
     info: `Mystical Majesty 생화는 풍부한 색감과 매혹적인 매력으로 유명한 짙은 보라색과 파란색 꽃이 특징입니다. 계절에 따라 무성한 잎사귀로 포인트를 준 이 생화는 신비롭고 우아한 분위기를 자아냅니다.`,
+    category: 'fresh flowers',
   },
   {
     id: 10,
@@ -76,5 +85,6 @@ export const freshFlowersProducts: ProductType[] = [
     price: 70,
     imgPath: IMG_PATH.FreshFlowers.BlazingBlassoms,
     info: `Blazing Blossoms 생화는 강렬한 붉은색과 오렌지색 꽃이 돋보이는 생화로, 불 같은 색감과 대담한 존재감이 특징입니다. 계절의 푸르름과 어우러진 이 생화는 어떤 환경에서도 에너지와 따뜻함을 선사합다.`,
+    category: 'fresh flowers',
   },
 ];
--- src/mocks/data/freshenersDatas.ts
+++ src/mocks/data/freshenersDatas.ts
@@ -2,7 +2,7 @@ import { ProductType, ProductBgType } from 'src/@types/product';
 import { IMG_PATH } from '@constants/imgPath';
 
 export const freshenersBg: ProductBgType = {
-  title: 'Freshener',
+  title: 'Fresheners',
   imgPath: IMG_PATH.Fresheners.FreshenersBg,
 };
 
@@ -13,6 +13,7 @@ export const freshenersProducts: ProductType[] = [
     price: 70,
     imgPath: IMG_PATH.Fresheners.CedarLavender,
     info: `Snowfall 부케는 은은한 향기로 유명한 우아한 화이트 장미와 계절의 푸르름이 어우러져 순수한 아름다움을 더욱 돋보이게 합니다. 이 절묘한 부케는 어떤 상황에서도 세련미와 우아함을 더합니다.`,
+    category: 'fresheners',
   },
   {
     id: 2,
@@ -20,6 +21,7 @@ export const freshenersProducts: ProductType[] = [
     price: 70,
     imgPath: IMG_PATH.Fresheners.HimalayanSaltRosewater,
     info: `Dawn's Delight 부케는 매혹적인 색감과 은은한 향기로 유명한 복숭아와 핑크 장미를 선보입니다. 계절의 잎사귀로 포인트를 준 이 부케는 어떤 환경에서도 따뜻함과 매력을 선사합니다.`,
+    category: 'fresheners',
   },
   {
     id: 3,
@@ -27,6 +29,7 @@ export const freshenersProducts: ProductType[] = [
     price: 70,
     imgPath: IMG_PATH.Fresheners.WildMintEucalyptus,
     info: `Pink Elegance 부케는 선명한 색상과 달콤한 향기로 사랑받는 아름다운 핑크 장미가 특징입니다. 계절의 푸르름과 어우러진 이 부케는 우아함과 세련미를 발산합니다.`,
+    category: 'fresheners',
   },
   {
     id: 4,
@@ -34,6 +37,7 @@ export const freshenersProducts: ProductType[] = [
     price: 70,
     imgPath: IMG_PATH.Fresheners.Grapefruit,
     info: `Rustic Charm 부케는 흙빛 장미와 야생화를 결합하여 자연스럽고 매력적인 아름다움을 연출합니다. 계절의 단풍으로 더욱 돋보이는 이 부케는 시골의 아름다움을 떠올리게 합니다.`,
+    category: 'fresheners',
   },
   {
     id: 5,
@@ -41,6 +45,7 @@ export const freshenersProducts: ProductType[] = [
     price: 70,
     imgPath: IMG_PATH.Fresheners.OceanMist,
     info: `Autumn Symphony 부케는 따뜻한 톤의 장미와 제철 단풍이 조화롭게 어우러져 가을의 정수를 담아낸 것이 특징입니다. 가을의 생생한 색감과 아늑한 느낌을 실내에서도 느낄 수 있는 부케입니다.`,
+    category: 'fresheners',
   },
   {
     id: 6,
@@ -48,5 +53,6 @@ export const freshenersProducts: ProductType[] = [
     price: 70,
     imgPath: IMG_PATH.Fresheners.Driftwood,
     info: `Rosy Delight 부케는 아름다움과 은은한 향기로 유명한 데이비드 오스틴 장미를 엄선하여 선보입니다. 제철 잎사귀와 어우러져 이 아름다운 꽃의 숭고한 매력을 더욱 돋보이게 하는 부케입니다.`,
+    category: 'fresheners',
   },
 ];
