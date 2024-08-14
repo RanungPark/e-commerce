f302033f6d389865cf9d11548dcdfc1c2af2528a
woong <ckehfqkr302@naver.com>
2024-08-15 01:21:05 +0900

    cartItemCardList 스토리북

---
+++ src/stories/lists/CartItemCardList.stories.tsx
@@ -0,0 +1,26 @@
+import CartItemCardList from '@components/lists/CartItemCardList';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof CartItemCardList> = {
+  title: 'Lists/CartItemCardList',
+  component: CartItemCardList,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    hasTextButton: {
+      control: 'boolean',
+      description: 'CartItemCard 텍스트 버튼 유무',
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    hasTextButton: true,
+  },
+};
