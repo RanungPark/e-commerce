f24ed7f326b7987ed2cd9a0844902a924d0e2326
woong <ckehfqkr302@naver.com>
2024-08-17 00:26:31 +0900

    form 컴포넌트로 분리

---
+++ src/components/forms/CheckoutForm.tsx
@@ -0,0 +1,83 @@
+import PrimaryButton from '@components/buttons/PrimaryButton';
+import DefaultTextField from '@components/textfields/DefaultTextField';
+import DatePicker from '@components/utilities/DatePicker';
+import DropDown from '@components/utilities/DropDown';
+import { InputTextsType } from '@data/inputDatas';
+import { mixins } from '@styles/Mixin';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import styled from 'styled-components';
+import { v4 as uuidv4 } from 'uuid';
+
+interface CheckoutFormProps {
+  onSubmit: () => void;
+  checkoutFormDatas: InputTextsType[];
+}
+
+const CheckoutForm = ({ onSubmit, checkoutFormDatas }: CheckoutFormProps) => {
+  const [selectValue, setSelectValue] = useState('');
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+
+  const handleDateChange = (date: Date) => {
+    setSelectedDate(date);
+  };
+
+  const { register, handleSubmit } = useForm();
+
+  return (
+    <CheckoutFormWrapper onSubmit={handleSubmit(onSubmit)}>
+      {checkoutFormDatas.map(
+        ({ component, registerValue, placeholder, ...rest }) => {
+          if (component === 'textField') {
+            return (
+              <DefaultTextField key={uuidv4()}>
+                <input
+                  {...register(registerValue, {
+                    required: rest?.required,
+                    pattern: rest.pattern && {
+                      value: rest.pattern.value,
+                      message: rest.pattern.message,
+                    },
+                  })}
+                  placeholder={placeholder}
+                  aria-label={registerValue}
+                />
+              </DefaultTextField>
+            );
+          } else if (component === 'datePicker') {
+            return (
+              <DatePicker
+                placeholder={placeholder}
+                value={selectedDate}
+                key={uuidv4()}
+                onChange={handleDateChange}
+              />
+            );
+          } else if (component === 'selectBox') {
+            return (
+              <DropDown
+                value={selectValue}
+                onChange={setSelectValue}
+                options={rest.options ? rest.options : []}
+                placeholder={placeholder}
+                key={uuidv4()}
+              />
+            );
+          }
+        }
+      )}
+      <PrimaryButton onClick={handleSubmit(onSubmit)}>
+        Continue to Payment
+      </PrimaryButton>
+    </CheckoutFormWrapper>
+  );
+};
+
+const CheckoutFormWrapper = styled.form`
+  ${mixins.flexBox({ direction: 'column', align: 'start' })}
+
+  gap: 16px;
+  width: 100%;
+`;
+
+export default CheckoutForm;
---
+++ src/components/forms/LoginIdForm.tsx
@@ -0,0 +1,85 @@
+import PrimaryButton from '@components/buttons/PrimaryButton';
+import DefaultTextField from '@components/textfields/DefaultTextField';
+import { REG_PHONE } from '@constants/reg';
+import { loginStateType } from '@pages/LoginPage';
+import { mixins } from '@styles/Mixin';
+import { useMutation } from '@tanstack/react-query';
+import { fetchLogin } from '@utils/api';
+import { useForm } from 'react-hook-form';
+import styled from 'styled-components';
+
+interface LoginIdFormProps {
+  onSubmit: (e: loginStateType) => void;
+  setUsername: (e: string) => void;
+}
+
+type LoginIdFormType = {
+  username: string;
+};
+
+const LoginIdForm = ({ onSubmit, setUsername }: LoginIdFormProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm<LoginIdFormType>();
+
+  const LoginIdMutation = useMutation({
+    mutationFn: (username: string) => fetchLogin(username),
+    onSuccess: data => {
+      const { message } = data;
+      const username = getValues().username;
+
+      if (message === '로그인 진행') {
+        setUsername(username);
+        onSubmit('join');
+      } else if (message === '회원가입 진행') {
+        setUsername(username);
+        onSubmit('signup');
+      }
+    },
+    onError: error => {
+      console.error('Login failed:', error);
+    },
+  });
+
+  const onLoginValid = ({ username }: LoginIdFormType) => {
+    LoginIdMutation.mutate(username);
+  };
+
+  return (
+    <LoginIdFormWrapper>
+      <DefaultTextField
+        inputState={errors.username ? 'error' : undefined}
+        hasLabel={true}
+        label="휴대폰 번호를 사용하여 가입 또는 로그인하기"
+        hasHelpMessage={true}
+        helpMessage={errors.username?.message}
+      >
+        <input
+          {...register('username', {
+            required: 'ID를 입력해주세요',
+            pattern: {
+              value: REG_PHONE,
+              message: `'-' 없이 전화번호를 입력해주세요.`,
+            },
+          })}
+          placeholder="+82 XXX XXX XXX"
+          aria-label="username"
+        />
+      </DefaultTextField>
+      <PrimaryButton onClick={handleSubmit(onLoginValid)}>
+        continue
+      </PrimaryButton>
+    </LoginIdFormWrapper>
+  );
+};
+
+const LoginIdFormWrapper = styled.form`
+  ${mixins.flexBox({ direction: 'column', align: 'start' })}
+  gap: 32px;
+  width: 100%;
+`;
+
+export default LoginIdForm;
