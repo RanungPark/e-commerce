5f1894d29c43a8e0cb5257a11435d7f061aee3be
woong <ckehfqkr302@naver.com>
2024-08-19 15:00:17 +0900

    스켈레톤 컴포넌트 스토리북

---
+++ src/stories/skeletons/TitleCardSkeleton.stories.tsx
@@ -0,0 +1,23 @@
+import TitleCardSkeleton from '@components/skeletons/TitleCardSkeleton';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof TitleCardSkeleton> = {
+  title: 'Skeletons/TitleCardSkeleton',
+  component: TitleCardSkeleton,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  decorators: [
+    Story => (
+      <div style={{ width: '784px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
