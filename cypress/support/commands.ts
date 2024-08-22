6d91f6292875f51cbe321dd2740dd026a05a4499
woong <ckehfqkr302@naver.com>
2024-08-22 20:52:08 +0900

    cypress commands 추가

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,37 +1,53 @@
-/// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+Cypress.Commands.add('login', ({ username, password }) => {
+  cy.visit('/');
+  cy.visit('/login');
+
+  cy.get('[data-cy=phoneInput]').as('phoneInput');
+  cy.get('@phoneInput').type(username);
+
+  cy.get('[data-cy=phoneBtn]').should('exist').click();
+
+  cy.get('[data-cy=passwordInput]').as('passwordInput');
+  cy.get('@passwordInput').type(password);
+
+  cy.get('[data-cy=passwrodBtn]').should('exist').click();
+});
+
+Cypress.Commands.add('addToCart', () => {
+  cy.visit('/');
+
+  cy.get('[data-cy=action_0]')
+    .find('[data-cy=actionCardBtn]')
+    .should('exist')
+    .click();
+
+  cy.get('[data-cy=fresh_flowers_1]')
+    .as('freshFlower1')
+    .should('exist')
+    .click();
+
+  cy.get('[data-cy=plusBtn]').as('plusBtn').should('exist').click();
+
+  cy.get('[data-cy=addBtn]').as('addBtn').should('exist').click();
+});
+
+Cypress.Commands.add('addToCartSub', () => {
+  cy.get('[data-cy=fresh_flowers_4_CarouseBtn]').as('freshFlower4CarouseBtn');
+  cy.get('@freshFlower4CarouseBtn').should('exist').click();
+
+  cy.url().should('include', '/categories/fresh_flowers/products/4');
+
+  cy.get('[data-cy=plusBtn]').as('plusBtn');
+  cy.get('@plusBtn').should('exist').click();
+
+  cy.get('[data-cy=addBtn]').as('addBtn');
+  cy.get('[data-cy=addBtn]').should('exist').click();
+
+  cy.get('[data-cy=aroma_candles_1]').as('aromaCandles1');
+  cy.get('@aromaCandles1').should('exist').click();
+
+  cy.url().should('include', '/categories/aroma_candles/products/1');
+
+  cy.get('[data-cy=addBtn]').as('addBtn');
+  cy.get('[data-cy=addBtn]').should('exist').click();
+});
